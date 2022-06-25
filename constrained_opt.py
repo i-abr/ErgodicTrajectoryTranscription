7fb3bd19f72b03ba7d9b07a85221d2650800a52c
ia-mac <iabr4073@gmail.com>
2022-06-24 20:12:18 -0400

    args works, but mpc implementation of erg search acting weird, need to investigate further

--- constrained_opt.py
+++ constrained_opt.py
@@ -4,43 +4,43 @@ from jax import grad, jacfwd, vmap, jit, hessian
 import jax.numpy as np
 
 class AugmentedLagrangian(object):
-    def __init__(self, x0, loss, eq_constr, ineq_constr, step_size=1e-3, c=1.0):
+    def __init__(self, x0, loss, eq_constr, ineq_constr, args, step_size=1e-3, c=1.0):
         self.loss = loss 
         self.eq_constr   = eq_constr
         self.ineq_constr = ineq_constr
-        _eq_constr       = eq_constr(x0)
-        _ineq_constr     = ineq_constr(x0)
+        _eq_constr       = eq_constr(x0, args)
+        _ineq_constr     = ineq_constr(x0, args)
         lam = np.zeros(_eq_constr.shape)
         mu  = np.zeros(_ineq_constr.shape)
         self.solution = {'x' : x0, 'lam' : lam, 'mu' : mu}
-        def lagrangian(solution):
+        def lagrangian(solution, args):
             x   = solution['x']
             lam = solution['lam']
             mu  = solution['mu']
-            _eq_constr   = eq_constr(x)
-            _ineq_constr = ineq_constr(x)
-            return loss(x) \
+            _eq_constr   = eq_constr(x, args)
+            _ineq_constr = ineq_constr(x, args)
+            return loss(x, args) \
                 + np.sum(lam * _eq_constr + 0.5 * (_eq_constr)**2) \
                     + 0.5 * np.sum(np.maximum(0., mu + _ineq_constr)**2 - mu**2)
             # return loss(x) + np.sum(lam * _eq_constr + 0.5 * (_eq_constr)**2) + 0.5 * np.sum(mu * np.maximum(0., mu + _ineq_constr))
-            
+
         dldth = jit(grad(lagrangian))
         @jit
-        def step(solution):
-            _dldx = dldth(solution)
+        def step(solution, args):
+            _dldx = dldth(solution, args)
             _eps = np.linalg.norm(_dldx['x'])
             solution['x']   = solution['x'] - step_size * _dldx['x']
-            solution['lam'] = solution['lam'] + c*eq_constr(solution['x'])
-            solution['mu']  = np.maximum(0, solution['mu'] + c*ineq_constr(solution['x']))
+            solution['lam'] = solution['lam'] + c*eq_constr(solution['x'], args)
+            solution['mu']  = np.maximum(0, solution['mu'] + c*ineq_constr(solution['x'], args))
             # theta['lam'] = theta['lam'] + step_size * _dldth['lam']
             # theta['mu']  = theta['mu'] + step_size * _dldth['mu']
             return solution, _eps
         self.lagrangian = lagrangian
         self.grad_lagrangian = dldth
         self.step = step
-    def solve(self, max_iter=10000, eps=1e-3):
+    def solve(self, args, max_iter=10000, eps=1e-3):
         for k in range(max_iter):
-            self.solution, _eps = self.step(self.solution)
+            self.solution, _eps = self.step(self.solution, args)
             if _eps < eps:
                 print('done in ', k, ' iterations')
                 break
