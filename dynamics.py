e33af9d9530e1b0d9eb8497dcb4bc3f29cf5daa2
ia-mac <iabr4073@gmail.com>
2022-07-10 15:35:47 -0400

    pushing up terminal condition

---
+++ dynamics.py
@@ -0,0 +1,19 @@
+import jax.numpy as np
+
+class SingleIntegrator(object):
+    def __init__(self) -> None:
+        self.dt = 0.1
+        self.n = 2
+        self.m = 2
+        def f(x, u):
+            B = np.array([
+                [1.0,0.],
+                [0.,1.]
+            ])
+            # B = np.array([
+            #     [np.cos(x[2]), 0.,],
+            #     [np.sin(x[2]), 0.],
+            #     [0., 1.]
+            # ])
+            return x + self.dt*B@u
+        self.f = f
--- opt_solver.py
+++ opt_solver.py
@@ -4,8 +4,16 @@ from jax import grad, jacfwd, vmap, jit, hessian
 from jax.flatten_util import ravel_pytree
 import jax.numpy as np
 
+
+#     avg_sq_grad = jnp.zeros_like(x0)
+#     return x0, avg_sq_grad
+#   def update(i, g, state):
+#     x, avg_sq_grad = state
+#     avg_sq_grad = avg_sq_grad * gamma + jnp.square(g) * (1. - gamma)
+
 class AugmentedLagrangian(object):
     def __init__(self, x0, loss, eq_constr, ineq_constr, args=None, step_size=1e-3, c=1.0):
+        self.def_args = args
         self.loss = loss 
         self.eq_constr   = eq_constr
         self.ineq_constr = ineq_constr
@@ -15,41 +23,45 @@ class AugmentedLagrangian(object):
         mu  = np.zeros(_ineq_constr.shape)
         self._x_shape = x0.shape
         self.solution = {'x' : x0, 'lam' : lam, 'mu' : mu}
-        self._flat_solution, self._unravel = ravel_pytree(self.solution)
-        def lagrangian(flat_solution, args):
-            solution = self._unravel(flat_solution)
+        self.avg_sq_grad = np.zeros_like(x0)
+        # self._flat_solution, self._unravel = ravel_pytree(self.solution)
+        def lagrangian(solution, args):
+            # solution = self._unravel(flat_solution)
             x   = solution['x']
             lam = solution['lam']
             mu  = solution['mu']
             _eq_constr   = eq_constr(x, args)
             _ineq_constr = ineq_constr(x, args)
             return loss(x, args) \
-                - np.sum(lam * _eq_constr) \
-                + np.sum(0.5*(_eq_constr)**2) \
-                + 0.5 * np.sum(np.maximum(0., mu + _ineq_constr)**2) \
-                + np.sum(mu**2)
+                + np.sum(lam * _eq_constr + 0.5 * (_eq_constr)**2) \
+                + 0.5 * np.sum(np.maximum(0., mu + _ineq_constr)**2 - mu**2)
+
         dldx = jit(grad(lagrangian))
-        dl2dx2 = jacfwd(dldx)
+        gamma=0.9
+        eps=1e-8
         @jit
-        def step(flat_solution, args):
-            _dldx   = dldx(flat_solution, args)
-            # _dl2dx2 = dl2dx2(flat_solution, args)
-            flat_solution = flat_solution - step_size * _dldx
-            _eps = np.linalg.norm(_dldx)
-            # solution['x']   = solution['x'] - step_size * _dldx['x']
-            # solution['lam'] = solution['lam'] + c*eq_constr(solution['x'], args)
-            # solution['mu']  = np.maximum(0, solution['mu'] + c*ineq_constr(solution['x'], args))
-            # theta['lam'] = theta['lam'] + step_size * _dldth['lam']
-            # theta['mu']  = theta['mu'] + step_size * _dldth['mu']
-            return flat_solution, _eps
-        self.lagrangian = lagrangian
+        def step(solution, args, avg_sq_grad):
+            _dldx   = dldx(solution, args)
+            _eps    = np.linalg.norm(_dldx['x'])
+            avg_sq_grad = avg_sq_grad * gamma + np.square(_dldx['x']) * (1. - gamma)
+            solution['x']   = solution['x'] - step_size * _dldx['x'] / np.sqrt(avg_sq_grad + eps)
+            solution['lam'] = solution['lam'] + c*eq_constr(solution['x'], args)
+            solution['mu']  = np.maximum(0, solution['mu'] + c*ineq_constr(solution['x'], args))
+            return solution, _eps, avg_sq_grad
+
+        self.lagrangian      = lagrangian
         self.grad_lagrangian = dldx
         self.step = step
+
     def get_solution(self):
-        return self._unravel(self._flat_solution)
+        return self.solution
+        # return self._unravel(self._flat_solution)
+
     def solve(self, args=None, max_iter=10000, eps=1e-3):
+        if args is None:
+            args = self.def_args
         for k in range(max_iter):
-            self._flat_solution, _eps = self.step(self._flat_solution, args)
+            self.solution, _eps, self.avg_sq_grad = self.step(self.solution, args, self.avg_sq_grad)
             if _eps < eps:
                 print('done in ', k, ' iterations')
                 break
